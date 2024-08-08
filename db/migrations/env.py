b3224b2c69761092806d551f16036dba5d94c547
yura <lescovec@gmail.com>
2024-08-08 18:46:15 +0300

    add alembic

--- db/migrations/env.py
+++ db/migrations/env.py
@@ -11,12 +11,21 @@ from db.database import Base, Treads # noqa
 
 load_dotenv('.env')
 
+# db_url = "postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}".format(
+#     DB_USER=os.getenv('DB_USER'),
+#     DB_PASS=os.getenv('DB_PASS'),
+#     DB_HOST=os.getenv('DB_HOST'),
+#     DB_PORT=os.getenv('DB_PORT'),
+#     DB_NAME=os.getenv('DB_NAME'),
+# )
+
+
 db_url = "postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}".format(
-    DB_USER=os.getenv('DB_USER'),
-    DB_PASS=os.getenv('DB_PASS'),
-    DB_HOST=os.getenv('DB_HOST'),
-    DB_PORT=os.getenv('DB_PORT'),
-    DB_NAME=os.getenv('DB_NAME'),
+    DB_USER='postgres',
+    DB_PASS='JZOEtYSauBmuAzJjZiJQdvyDbZfdStxO',
+    DB_HOST='postgres-yshe.railway.internal',
+    DB_PORT='5432',
+    DB_NAME='railway',
 )
 
 config = context.config
