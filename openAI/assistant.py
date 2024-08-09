99ed55f8e2a1e127d08f76e5572d3c114a70a810
yura <lescovec@gmail.com>
2024-08-09 12:58:32 +0300

    add alembic

--- openAI/assistant.py
+++ openAI/assistant.py
@@ -1,16 +1,19 @@
 import os
-from openai import AsyncOpenAI
 
+from openai import AsyncOpenAI
 from openAI.openai_tools import tools
 from db.db_handler import add_tread_from_chat_id, get_tread_id_or_none
 
 
 async def get_answer_from_assistant(question: str, chat_id: int) -> str:
-
     client = AsyncOpenAI(api_key=os.getenv('API_TOKEN_OPENAI'))
-    
+
     assistant = await client.beta.assistants.create(
         name="assistant",
+        instructions="your main task is to find out my hobbies, "
+                     "find out my hobbies from the message history,"
+                     "don't ask me questions about my hobbies, "
+                     "use the save_value function to save my hobby",
         model="gpt-4o",
         tools=tools
 
@@ -27,10 +30,39 @@ async def get_answer_from_assistant(question: str, chat_id: int) -> str:
         content=question,
     )
 
-    await client.beta.threads.runs.create_and_poll(
+    run = await client.beta.threads.runs.create_and_poll(
         thread_id=tread_id,
         assistant_id=assistant.id,
     )
+
+    if run.status == 'requires_action':
+        tool_outputs = []
+
+        for tool in run.required_action.submit_tool_outputs.tool_calls:
+            if tool.function.name == "save_value":
+                tool_outputs.append({
+                    "tool_call_id": tool.id,
+                    "output": 'seve'
+                })
+            elif tool.function.name == "get_rain_probability":
+                tool_outputs.append({
+                    "tool_call_id": tool.id,
+                    "output": "0.06"
+                })
+            print(tool.function)
+
+        if tool_outputs:
+            try:
+                run = await client.beta.threads.runs.submit_tool_outputs_and_poll(
+                    thread_id=tread_id,
+                    run_id=run.id,
+                    tool_outputs=tool_outputs
+                )
+                print("Tool outputs submitted successfully.")
+            except Exception as e:
+                print("Failed to submit tool outputs:", e)
+        else:
+            print("No tool outputs to submit.")
 
     response = await client.beta.threads.messages.list(thread_id=tread_id)
     response = response.data[0].content[0].text.value
--- openAI/openai_tools.py
+++ openAI/openai_tools.py
@@ -1,23 +1,34 @@
 tools = [{
-          "type": "function",
-          "function": {
-            "name": "test",
-            "description": "рассказать сказку о колобке",
-            "parameters": {
-              "type": "object",
-              "properties": {
-                "location": {
-                  "type": "string",
-                  "description": "The city and state, e.g., San Francisco, CA"
-                },
-                "unit": {
-                  "type": "string",
-                  "enum": ["Celsius", "Fahrenheit"],
-                  "description": "The temperature unit to use. Infer this from the user's location."
+            "type": "function",
+            "function": {
+                "name": "save_value",
+                "description": "before replying to each message, analyze the story, find my hobby",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "location": {
+                            "type": "string",
+                            "description": "choose one hobby and write to me in one word"
+                        },
+                    },
+                    "required": ["location", ]
                 }
-              },
-              "required": ["location", "unit"]
             }
-          }
         },
-      ]
+        {
+            "type": "function",
+            "function": {
+                "name": "get_rain_probability",
+                "description": "Get the probability of rain for a specific location",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "location": {
+                            "type": "string",
+                            "description": "The city and state, e.g., San Francisco, CA"
+                        }
+                    },
+                    "required": ["location"]
+                }
+            }
+        }]
