eeecca8c0a6320c9331849045d5ac3e3351dd411
yura <lescovec@gmail.com>
2024-08-12 14:50:13 +0300

    add vector storage

--- openai_tool/assistant_tool.py
+++ openai_tool/assistant_tool.py
@@ -1,5 +1,7 @@
 import json
 
+from openai import OpenAI, AsyncOpenAI
+
 from openai_tool.completions_tool import validate_key_value
 from postgres_db.db_handler import update_key_value_by_chat_id
 from config import client
@@ -48,8 +50,16 @@ async def get_answer_from_assistant(question: str, chat_id: int, tread_id: str, 
         else:
             print("No tool outputs to submit.")
 
-    response = await client.beta.threads.messages.list(thread_id=tread_id)
+    messages = list(await client.beta.threads.messages.list(thread_id=tread_id, run_id=run.id))
 
-    response = response.data[0].content[0].text.value
+    messages = messages[0][1]
 
-    return response
+    message_content = messages[0].content[0].text
+    annotations = message_content.annotations
+    citations = []
+    for index, annotation in enumerate(annotations):
+        message_content.value = message_content.value.replace(annotation.text, '')
+        if file_citation := getattr(annotation, "file_citation", None):
+            cited_file = await client.files.retrieve(file_citation.file_id)
+            citations.append(f" [{cited_file.filename}]")
+    return message_content.value + "\n".join(citations)
